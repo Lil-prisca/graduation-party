3ca442c56a26a36425172db4c85841ac31508977
Oluwadarasimi Adeyinka <adeyinkaoluwadarasimi@gmail.com>
2025-11-01 13:09:49 +0100

    change time back to 3:30pm

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -117,7 +117,7 @@ const Second = () => {
             <Location
               pretext="When"
               reason="until the music stops"
-              space="JANUARY | 28 | 4PM"
+              space="JANUARY | 28 |3:30PM"
             />
             <div className="loca">
               <div className="prefix">Where</div>
